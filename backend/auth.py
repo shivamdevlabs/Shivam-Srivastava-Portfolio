26de0458474901d9730fc4f072c95f7ffe12e629
Shivam Srivastava <shivamsri142@gmail.com>
2026-07-02 01:27:41 +0530

    update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime, timedelta
 from typing import Optional
 from jose import JWTError, jwt
-from passlib.context import CryptContext
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
